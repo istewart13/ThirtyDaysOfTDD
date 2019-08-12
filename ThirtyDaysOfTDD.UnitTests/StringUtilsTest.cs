da6c95062382f86fdf792bd45e8481ea4b2397fa
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-08-12 12:35:42 +0100

    Add second test for complex sentence

--- ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
+++ ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
@@ -20,5 +20,21 @@ namespace ThirtyDaysOfTDD.UnitTests
             // assert
             Assert.AreEqual(expectedResult, result);
         }
+
+        [Test]
+        public void ShouldBeAbleToCountNumberOfLettersInComplexSentence()
+        {
+            // arrange
+            var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+            var characterToScanFor = "n";
+            var expectedResult = 5;
+            var stringUtils = new StringUtils();
+
+            // act
+            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
