1f9dcb38309ec150b1a8aea88a6d3df55ee19d43
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2019-08-12 11:38:40 +0100

    Add FindNumberOfOccurrences method

---
+++ ThirtyDaysOfTDD/StringUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThirtyDaysOfTDD
+{
+    public class StringUtils
+    {
+        public int FindNumberOfOccurences(string sentenceToScan, string characterToScanFor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
