354f78adf9f5c1e303c32fea6065a1ee33ec5746
Iain Stewart <iain.stewart08@gmail.com>
2019-08-15 22:56:10 +0100

    Refactor to create string util in setup method

--- ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
+++ ThirtyDaysOfTDD.UnitTests/StringUtilsTest.cs
@@ -5,6 +5,14 @@ namespace ThirtyDaysOfTDD.UnitTests
     [TestFixture]
     public class StringUtilsTest
     {
+        private StringUtils _stringUtils;
+
+        [OneTimeSetUp]
+        public void SetupStringUtilTests()
+        {
+            _stringUtils = new StringUtils();
+        }
+
         [Test]
         public void ShouldBeAbleToCountNumberOfLettersInSimpleSentence()
         {
@@ -12,10 +20,9 @@ namespace ThirtyDaysOfTDD.UnitTests
             var sentenceToScan = "TDD is awesome!";
             var characterToScanFor = "e";
             var expectedResult = 2;
-            var stringUtils = new StringUtils();
 
             // act
-            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+            int result = _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
 
             // assert
             Assert.AreEqual(expectedResult, result);
@@ -28,10 +35,9 @@ namespace ThirtyDaysOfTDD.UnitTests
             var sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
             var characterToScanFor = "n";
             var expectedResult = 5;
-            var stringUtils = new StringUtils();
 
             // act
-            int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+            int result = _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
 
             // assert
             Assert.AreEqual(expectedResult, result);
@@ -43,10 +49,9 @@ namespace ThirtyDaysOfTDD.UnitTests
             // arrange
             var sentenceToScan = "This test should throw an exception";
             var characterToScanFor = "xx";
-            var stringUtils = new StringUtils();
 
             // assert
-            Assert.That(() => stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor), Throws.ArgumentException);
+            Assert.That(() => _stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor), Throws.ArgumentException);
         }
     }
 }
